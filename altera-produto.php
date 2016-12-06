2756cb21eabf616507123e79ce61d45eb70423d1
FBertanha <bertanhahp@hotmail.com>
2016-12-05 22:35:28 -0200

    Aula 1

--- altera-produto.php
+++ altera-produto.php
@@ -1,26 +1,28 @@
 <?php
   require_once('header.php');
-  //require_once('conecta.php');
   require_once('banco-produto.php');
+  require_once('class/Produto.php');
 
-  $id = $_POST['id_produto'];
-  $produto = $_POST['produto'];
-  $preco = $_POST['preco'];
-  $descricao = $_POST['descricao'];
-  $categoria = $_POST['categoria_id'];
-  if(array_key_exists('usado', $_POST)) {
-    $usado = 1;
+  $produto = new Produto();
+  $produto->id = $_POST['id_produto'];
+  $produto->nome = $_POST['produto'];
+  $produto->preco = $_POST['preco'];
+  $produto->descricao = $_POST['descricao'];
+  $produto->categoria_id = $_POST['categoria_id'];
+
+  if(isset($_POST['usado'])) {
+    $produto->usado = 1;
   }else {
-    $usado = 0;
+    $produto->usado = 0;
   }
 
-  if(alteraProduto($conexao, $id, $produto, $preco, $descricao, $categoria, $usado)) {
+  if(alteraProduto($conexao, $produto)) {
 ?>
-    <p class="text-success"> O Produto <?=$produto?> foi alterado.</p>
+    <p class="text-success"> O Produto <?=$produto->nome?> foi alterado.</p>
     <?php } else {
       $msg = mysqli_error($conexao);
     ?>
-      <p class="text-danger"> O Produto <?=$produto?> não foi alterado. <?=$msg?> </p>
+      <p class="text-danger"> O Produto <?=$produto->nome?> não foi alterado. <?=$msg?> </p>
 <?php
       }
 
